65a032dfbe7d388b81a4c973bb4c1a2497537540
Jomark Pangan <yourname@example.com>
2020-06-07 16:53:12 +0800

    2nd commit, WIP

--- notes.js
+++ notes.js
@@ -1,39 +1,139 @@
-//Factory Functions returns an object.
-//The same as constructor but doesnt use the word 'new'
-//declare the variables to be created
-const playerFactory = (name, sprite) => {
+/* Conditions
 
-    const sayName = () => console.log(`My Name is ${name}`);
-    return { name, sprite, sayName };
+    1. Store the gameboard as an array inside a gameboard object
+    2. Players are stored in an object
+    3. game flow control is an object
+    4. main goal is to have as little global code as possible.
+    5. try to tuck everything on modules and factories.
+        a. RULE OF THUMB: if you need One of something: module.
+            if you need multiple of something: factory
+    6. Build the functions that allow players to add marks to a specific spot on the board.
+    7. Build the logic that checks for when the game is over! Should check for 3-in-a-row 
+        and a tie.
+    8. Clean up the interface to allow players to put in their names, include a button to 
+        start/restart the game and add a display element that congratulates the winning player!
+    9. 
+
+*/
+
+/* My Plan
+
+    1. A function which will create the board using DOM
+    2. clear the board, and set the first player to move
+    3. if the player clicks on a specific cell, mark it with the sprite
+    4. check if the player has the winning combinations, if not, player two will move
+
+
+*/
+
+
+
+
+
+const Game = () => {
+
+    let turnCount = 0;
+    const cellsBtn = document.querySelectorAll(".board-container > .cells");
+    let player1;
+    let player2;
+
+    const createBoard = () => {
+
+        const cellContainer = document.querySelector('.cells');
+    
+        for (let i = 0; i < 3 * 3; i++) {
+    
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.textContent = i + 1;
+            cellContainer.appendChild(cell);
+    
+        };
+    };
+
+
+    const getPlayerNames = () => {
+
+        let playerOneName = prompt("Enter Player Name(X)");
+        let playerTwoName = prompt("Enter Player Name(O)");
+
+        const playerOne = playerFactory(playerOneName, "X");
+        const playerTwo = playerFactory(playerTwoName, "O");
+        createBoard();
+        return{playerOne,playerTwo};
+    };
+
+
+    const checkVictory = (player,moveHistory) => {
+
+        let winConditions = [[1,2,3], [4,5,6], [7,8,9], 
+                            [1,4,7],[2,5,8], [3,6,9], [1,5,9], [3,5,7]]; 
+                
+        turnCount >= 9 ? console.log("Tie!") : console.log("tuloy");
+
+        for(let i = 0; i < winConditions.length; i++){
+
+            if(winConditions[i].includes(moveHistory)){
+                console.log("WIN!");
+                break;
+            }
+        }
+
+    };
+
+    const gameTurn = () => {
+
+        turnCount += 1;
+      
+        let currentPlayer = turnCount % 2 == 1 ? player1:player2;
+        console.log("turns: " + turnCount);
+        checkVictory(player1, player1.moveHistory);
+        checkVictory(player2, player2.moveHistory);
+        getMove(currentPlayer);
+ 
+
+    };
+
+    const getMove = (player) => {
+
+        console.log("current player: " + player.name);
+
+        cellsBtn.forEach(cell => {
+
+            cell.addEventListener("click", function(e){
+
+                let cellSelected = e.target.innerText;
+                player.moveHistory.push(cellSelected);
+                console.log("moves:" + player.moveHistory);
+                gameTurn();
+            });
+        });
+
+
+    };
+
+    const getPlayers = (() => {
+
+        let players = getPlayerNames();
+        player1 = players.playerOne;
+        player2 = players.playerTwo;
+
+        gameTurn();
+
+    })();
+    
+
 };
 
-const maxi = playerFactory("jomark", "X");
-
-
-//Immediately Invoked Function Expression (IIFE)
-//Immediately calls the function for privacy
-const calculator = (() => {
-
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-    const mul = (a, b) => a * b;
-    const div = (a, b) => a / b;
-
-    return { add, sub, mul, div };
-
-})();
-
-const a = calculator.add(5, 6);
 
 
 
-const myFunction = (() => {
-    
-    console.log("Hello!")
-})();
+const playerFactory = (name, sprite) => {
 
+    let moveHistory = [];
+    return { name, sprite, moveHistory };
+};
 
-let myModule = 
-
+Game();
 
 
--- script.js
+++ script.js
@@ -1,128 +1,103 @@
-/* Conditions
 
-    1. Store the gameboard as an array inside a gameboard object
-    2. Players are stored in an object
-    3. game flow control is an object
-    4. main goal is to have as little global code as possible.
-    5. try to tuck everything on modules and factories.
-        a. RULE OF THUMB: if you need One of something: module.
-            if you need multiple of something: factory
-    6. Build the functions that allow players to add marks to a specific spot on the board.
-    7. Build the logic that checks for when the game is over! Should check for 3-in-a-row 
-        and a tie.
-    8. Clean up the interface to allow players to put in their names, include a button to 
-        start/restart the game and add a display element that congratulates the winning player!
-    9. 
+const createPlayer = (name, sprite) => {
 
-*/
+    const moveHistory = [];
 
-/* My Plan
+    return { name, sprite, moveHistory };
 
-    1. A function which will create the board using DOM
-    2. clear the board, and set the first player to move
-    3. if the player clicks on a specific cell, mark it with the sprite
-    4. check if the player has the winning combinations, if not, player two will move
-
-
-*/
-
-
-
-
+};
 
 const Game = () => {
 
+    const gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; //the gameboard in array form
+    const gameContainer = document.querySelector('.board-container'); //reference to the board-container tag
+    let player1;
+    let player2;
+    let currentPlayer;
     let turnCount = 0;
-    
-    const getPlayerNames = () => {
 
-        let playerOneName = prompt("Enter Player Name(X)");
-        let playerTwoName = prompt("Enter Player Name(O)");
+    const renderBoard = () => {    //A function which will render the contents of the gameboard
 
-        const playerOne = playerFactory(playerOneName, "X");
-        const playerTwo = playerFactory(playerTwoName, "O");
+        gameContainer.innerHTML = "";
+        for (let x = 1; x < gameBoard.length; x++) {
 
-        return{playerOne,playerTwo};
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.textContent = gameBoard[x];
+            cell.addEventListener("click", function (e) {
+
+                let move = e.target.innerText;
+                getMove(currentPlayer, move);
+            });
+
+            gameContainer.appendChild(cell);
+
+        };
     };
 
-    const checkVictory = (player,moveHistory) => {
 
-        let winConditions = [[1,2,3], [4,5,6], [7,8,9], 
-                            [1,4,7],[2,5,8], [3,6,9], [1,5,9], [3,5,7]]; 
-                
-        turnCount >= 9 ? console.log("Tie!") : console.log("tuloy");
 
-        for(let i = 0; i < winConditions.length; i++){
+    const getMove = (player, move) => {
 
-            console.log(winConditions[i],moveHistory);
-            if(winConditions[i].includes(moveHistory)){
+        player.moveHistory.push(move);
 
-                console.log("WIN!");
+        let index = gameBoard.indexOf(parseInt(move));
+        gameBoard.splice(parseInt(index), 1, player.sprite);
+        currentPlayer.moveHistory.sort((a, b) => a - b);
+        gameTurn();
+    }
+
+
+    const gameTurn = () => {
+
+        renderBoard();
+        turnCount++;
+        currentPlayer = turnCount % 2 == 1 ? player1 : player2;
+        currentPlayer.moveHistory.sort((a, b) => a - b);
+        checkVictory(player1.moveHistory);
+        checkVictory(player2.moveHistory);
+        console.log(`current player: ${currentPlayer.name}`);
+        console.log(`sorted history: ${currentPlayer.moveHistory}`);
+
+    };
+
+    const checkVictory = (player) => {
+
+        let winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+        [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 6, 9], [3, 5, 7]];
+
+
+        for (let i = 0; i < winningCombinations.length; i++) {
+
+            if (player.includes(winningCombinations[i][i])) {
+                console.log("okay");
+                if (player.includes(winningCombinations[i][i + 1])) {
+                    console.log("okay");
+                    if (player.includes(winningCombinations[i][i + 2])) {
+
+                        console.log("exit");
+                    }
+                }
+
             }
-        }
+        };
+
 
     };
 
-    const gameTurn = (p1,p2) => {
+    const getPlayerNames = (() => {  //Get the names of the players and create instance of player
 
-        turnCount += 1;
+        let p1name = prompt("Enter Name (X)");
+        let p2name = prompt("Enter name (O)");
+        player1 = createPlayer(p1name, "X");
+        player2 = createPlayer(p2name, "O");
 
-        let currentPlayer = turnCount % 2 == 1 ? p1:p2;
-        checkVictory(p1, p1.moveHistory);
-        checkVictory(p2, p2.moveHistory);
- 
-
-    };
-
-    const getPlayers = (() => {
-
-        let players = getPlayerNames();
-        
-        let player1 = players.playerOne;
-        let player2 = players.playerTwo;
-
-        gameTurn(player1,player2);
+        gameTurn();
 
     })();
 
-   
-  
-
-    const createBoard = () => {
-
-        const boardContainer = document.querySelector('.board-container');
-    
-        for (let i = 0; i < 3 * 3; i++) {
-    
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.textContent = i + 1;
-            boardContainer.appendChild(cell);
-    
-        };
-    };
-    
-    
-
-
 };
 
-const playerFactory = (name, sprite) => {
 
-    let moveHistory = [1,2,3];
-    return { name, sprite, moveHistory };
-};
 
 Game();
-
-
-
-
-
-
-
-
-
-
-
-
